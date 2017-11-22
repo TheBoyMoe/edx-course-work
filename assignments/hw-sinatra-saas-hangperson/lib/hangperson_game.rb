5bff3188fce5d36708421e160e919ddae14aa79c
theboymo <billfero@gmail.com>
2017-11-22 10:41:34 +0000

    impl hangperson game class, all specs passing

--- assignments/hw-sinatra-saas-hangperson/lib/hangperson_game.rb
+++ assignments/hw-sinatra-saas-hangperson/lib/hangperson_game.rb
@@ -1,4 +1,7 @@
 class HangpersonGame
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  # HangpersonGame encapsulates the game logic - the 'M' in 'MVC'
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
@@ -7,14 +10,69 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+  # map RESTful actions
+  # GET /show => show game state, allow player to enter guess => display correct & wrong guesses so far, may redirect to Win or Lose
+  # GET /new => display a form that includes a 'Start' new game button => generate POST /create
+  # POST /create => create new game => redirect to '/show'
+  # POST /guess => update game state with new guessed letter(provide a form) => redirect to '/show'
+  # GET /win => Show 'you win' page with button to start new game
+  # GET /lose => Show 'you lose' page with button to start new game
+
+
+  # A 'GET' can be accomplished by just typing a URL into the browser's address bar.
+  # A 'POST' can only happen when the user submits an HTML form (or an AJAX request).
+
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  # processes a guess and modifies the instance variables wrong_guesses and guesses
+  def guess(letter)
+    valid_letter(letter).downcase!
+    if !@guesses.include?(letter) && !@wrong_guesses.include?(letter)
+      @word.include?(letter)? @guesses << letter : @wrong_guesses << letter
+      return true
+    end
+    return false
+  end
+
+  # returns one of the symbols :win, :lose, or :play depending on the current game state
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= @word.length
+    return :win if @guesses.length >= @word.length
+    :play
+  end
+
+  # substitutes the correct guesses made so far into the word.
+  def word_with_guesses
+    return '-' * @word.length unless @guesses.length > 0
+    # @word.split('').map do |letter|
+    #   if guess(letter)
+
+    #   end
+    # end.join('')
+    current = ''
+    @word.split('').each do |letter|
+      if !@guesses.include? letter
+        current += '-'
+      else
+        @guesses.split('').each do |char|
+          if char == letter
+            current += char
+          end
+        end
+      end
+    end
+    current
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
+  #  retrieves a random word from a Web service
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -23,5 +81,15 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
+
+
+  private
+    def valid_letter(letter)
+      if !letter || letter.empty? || /[^a-z]/i =~ letter
+        raise ArgumentError, 'Invalid letter'
+      else
+        letter
+      end
+    end
 
 end
