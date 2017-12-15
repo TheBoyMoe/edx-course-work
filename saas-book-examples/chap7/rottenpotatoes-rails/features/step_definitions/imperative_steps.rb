9d21ad231472f0961d6bc1140fb6a4957fcee3bf
theboymo <billfero@gmail.com>
2017-12-15 14:19:42 +0000

    compare declarative vs imperative use of cucumber steps

---
+++ saas-book-examples/chap7/rottenpotatoes-rails/features/step_definitions/imperative_steps.rb
@@ -0,0 +1,4 @@
+# Then(/^I should see "([^"]*)" before "([^"]*)" on the RottenPotatoes home page sorted by title$/) do |arg1, arg2|
+# 	regexp = /#{string1}.*#{string2}/m #  /m means match across newlines
+# 	expect(page.body).to match(regexp)
+# end
