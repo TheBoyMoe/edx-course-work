cb653e123c80b1ae9af7915ae0cea8a661ff232b
theboymo <billfero@gmail.com>
2017-12-16 15:26:11 +0000

    test the tmdb api with a happy and sad path

--- saas-book-examples/chap8/rottenpotatoes-rails/app/models/movie.rb
+++ saas-book-examples/chap8/rottenpotatoes-rails/app/models/movie.rb
@@ -1,11 +1,19 @@
 class Movie < ActiveRecord::Base
 
+  # define our own exception
+  class Movie::InvalidKeyError < StandardError; end
+
   def self.get_ratings
     Movie.all.map {|movie| movie.rating}.uniq
   end
 
   def self.find_in_tmdb(search_term)
-    # call the api
+    # call the api - requires logic to raise an exception should an invalid api key be supplied
+    begin
+      Tmdb::Movie.find(search_term)
+    rescue Tmdb::InvalidApiKeyError
+       raise Movie::InvalidKeyError, 'Invalid API key'
+    end
   end
 
   def name_with_rating
--- saas-book-examples/chap8/rottenpotatoes-rails/spec/models/movie_spec.rb
+++ saas-book-examples/chap8/rottenpotatoes-rails/spec/models/movie_spec.rb
@@ -12,11 +12,35 @@ describe Movie do
 	end
 
 	describe "test movie model using factory_girl" do
-
 		it "includes rating and title in full name" do
 			movie = FactoryGirl.build(:movie, title: 'Milk', rating: 'R')
 			expect(movie.name_with_rating).to eq('Milk (R)')
 		end
+	end
+
+	describe "searching Tmdb  by keyword" do
+
+		context "with a valid API key" do
+			it "calls Tmdb with title keywords" do
+				expect(Tmdb::Movie).to receive(:find).with('Inception')
+				Movie.find_in_tmdb('Inception')
+			end
+		end
+
+		context "with invalid API key" do
+
+			before(:each){
+				# stubbing out the call using receive prevents the 'real' Tmdb::Movie.find method being called
+				allow(Tmdb::Movie).to receive(:find).and_raise(Tmdb::InvalidApiKeyError)
+			}
+
+			it "raises an InvalidKeyError" do
+				# place the call to find_in_tmdb in {} to prevent the exception raised from stopping the test
+				expect {Movie.find_in_tmdb('Inception')}
+						.to raise_error(Movie::InvalidKeyError)
+			end
+		end
+
 
 	end
 
