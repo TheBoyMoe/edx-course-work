9cd457eee1c08ac89ff7ddf9ba0b5a0c66d9faf9
theboymo <billfero@gmail.com>
2018-01-20 17:07:05 +0000

    re-organise repo

--- review/chap8/rottenpotatoes/spec/models/movie_spec.rb
+++ review/chap8/rottenpotatoes/spec/models/movie_spec.rb
@@ -43,7 +43,8 @@ describe Movie do
 
 	describe "searching Tmdb by keyword" do
 		it 'calls Tmdb with title keywords' do
-
+			expect(Tmdb::Movie).to receive(:find).with('Inception')
+			Movie.find_in_tmdb('Inception')
 		end
 	end
 
